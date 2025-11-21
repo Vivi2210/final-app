ef0773d507046fca47e58d128d9ac8bd0ba7dbae
Vivi2210 <vmoren2210@gmail.com>
2025-11-20 19:33:50 -0500

    final

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc, query, orderBy } from "firebase/firestore";
+import { db, serverTimestamp } from "./firebase";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  const tareasRef = collection(db, "tasks");
+
+  const cargarDatos = async () => {
+    const q = query(tareasRef, orderBy("createdAt", "desc"));
+    const snapshot = await getDocs(q);
+    const docs = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    setData(docs);
+  };
+
+  useEffect(() => {
+    cargarDatos();
+  }, []);
+
+  const guardarDatos = async () => {
+    const nuevaTarea = {
+      title: "nuueva tarrea",
+      description: "hacer la tarerra de desarrlollo",
+      studentId: "123456",
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp()
+    };
+    await addDoc(tareasRef, nuevaTarea);
+    cargarDatos();
+  };
+
+  const actualizarDatos = async (id) => {
+    const tareaRef = doc(db, "tasks", id);
+    await updateDoc(tareaRef, {
+      title: " actualizada",
+      description: "bien ",
+      updatedAt: serverTimestamp()
+    });
+    cargarDatos();
+  };
+
+  const eliminarDatos = async (id) => {
+    const tareaRef = doc(db, "tasks", id);
+    await deleteDoc(tareaRef);
+    cargarDatos();
+  };
+
+  return (
+
+
+    
+    <div className="App">
+      <h1>lista de tareass</h1>
+
+      <button onClick={guardarDatos}>Crear</button>
+
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <strong>{item.title}</strong><br />
+            {item.description && <em>{item.description}</em>}<br />
+            <button onClick={() => actualizarDatos(item.id)}>Actualizar</button>
+            <button onClick={() => eliminarDatos(item.id)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
